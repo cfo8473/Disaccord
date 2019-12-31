74ef5027d2c30fa7bed87d7d1db59f06884481b9
CF <cfong7411@gmail.com>
2019-12-30 17:49:01 -0800

    Add splash animations

--- frontend/components/greeting/greeting.jsx
+++ frontend/components/greeting/greeting.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import { Link } from 'react-router-dom'
 import { faDog, faLanguage, faKiwiBird, faDove, faCat} from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+// import svgs from "../../../app/assets/images/svgs";
+import imgs from "./splash_svgs";
 
 class Greeting extends React.Component {
   constructor(props) {
@@ -17,12 +19,27 @@ class Greeting extends React.Component {
               <FontAwesomeIcon icon={faDog} /> disaccord
             </div>
             <ul className="header-nav-center">
-              
-              <a className="splash-portfolio-link" href="https://cfong.io">Portfolio</a>
-              <a className="splash-portfolio-link" href="https://github.com/cfo8473">Github</a>
-              <a className="splash-portfolio-link" href="https://www.linkedin.com/in/chris-fong-33b6b3197/">LinkedIn</a>
-              <a className="splash-portfolio-link" href="https://angel.co/chris-fong-7">AngelList</a>
-
+              <a className="splash-portfolio-link" href="https://cfong.io">
+                Portfolio
+              </a>
+              <a
+                className="splash-portfolio-link"
+                href="https://github.com/cfo8473"
+              >
+                Github
+              </a>
+              <a
+                className="splash-portfolio-link"
+                href="https://www.linkedin.com/in/chris-fong-33b6b3197/"
+              >
+                LinkedIn
+              </a>
+              <a
+                className="splash-portfolio-link"
+                href="https://angel.co/chris-fong-7"
+              >
+                AngelList
+              </a>
             </ul>
 
             {/* <div className="header-icons">
@@ -30,23 +47,68 @@ class Greeting extends React.Component {
               <FontAwesomeIcon icon={faDove} /> - 
               <FontAwesomeIcon icon={faCat} />
             </div> */}
-            <ul className="header-nav-right"
-            >
-              <Link className="login-button" to="/login">Log In</Link>
-            </ul> 
-              {/* <div className="header-language"><FontAwesomeIcon icon={faLanguage} /></div> */}
+            <ul className="header-nav-right">
+              <Link className="login-button" to="/login">
+                Log In
+              </Link>
+            </ul>
+            {/* <div className="header-language"><FontAwesomeIcon icon={faLanguage} /></div> */}
           </nav>
         </header>
         <div className="greeting-text-padding">
           <h1 className="greeting-header-text">Welcome to Disaccord!</h1>
-          <h2 className="greeting-header-text">A full stack messaging app inspired by Discord</h2> 
-          <p className="greeting-text-use">Single-page application designed for chat messaging using the following technologies:</p>
-          <p className="greeting-text-use">Ruby, Rails, React, JavaScript, Node, Amazon S3 and PostGreSQL</p>
-          <p className="greeting-text-use">Please log in using the button on the top right. Demo Login is available.</p>
+          <h2 className="greeting-header-text">
+            A full stack messaging app inspired by Discord
+          </h2>
+          <p className="greeting-text-use">
+            Single-page application designed for chat messaging using the
+            following technologies:
+          </p>
+          <p className="greeting-text-use">
+            Ruby, Rails, React, JavaScript, Node, Amazon S3 and PostGreSQL
+          </p>
+          <p className="greeting-text-use">
+            Please log in using the button on the top right. Demo Login is
+            available.
+          </p>
         </div>
-        <div className="splashBackground"></div>
+        {/* <div className="splashBackground"></div> */}{" "}
+        <div className="splash-imgs">
+          <div className="shadow"></div>
+          {imgs.triangle}
+          {imgs.triangle}
+          {imgs.triangle}
+          {imgs.dot}
+          {imgs.dot}
+          {imgs.dot}
+          {imgs.dot}
+          {imgs.dot}
+          {imgs.circle}
+          {imgs.circle}
+          {imgs.circle}
+          {imgs.square}
+          {imgs.square}
+          {imgs.square}
+          {imgs.x}
+          {imgs.x}
+          {imgs.x}
+          {imgs.potion}
+          {imgs.bomb}
+          {imgs.coin}
+          {imgs.coin}
+          {imgs.cartridge}
+          {imgs.block}
+          {imgs.desktop}
+          {imgs.android}
+          {imgs.iphone}
+          {imgs.controller}
+          {imgs.laptop}
+          {imgs.headphones}
+        </div>
       </div>
-    ) : ( <div></div> )
+    ) : (
+      <div></div>
+    );
 
     return (
       <div>
