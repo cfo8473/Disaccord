2a3da25688cd6b74c34adbc0fdced69ffa73ec49
Wei <cfong7411@gmail.com>
2020-04-24 20:54:26 -0700

    Establish membership api

--- frontend/util/membership_api_util.js
+++ frontend/util/membership_api_util.js
@@ -5,3 +5,40 @@
 //   })
 
 // }
+
+export const fetchMemberships = () => {
+  return $.ajax({
+    method: "GET",
+    url: `/api/memberships`
+  })
+}
+
+export const fetchMembership = (id) => {
+  return $.ajax({
+    method: "GET",
+    url: `/api/memberships/${id}`
+  })
+}
+
+export const createMembership = membership => {
+  return $.ajax({
+    method: "POST",
+    url: `/api/memberships`,
+    data: { membership }
+  })
+}
+
+export const editMembership = membership => {
+  return $.ajax({
+    method: "PATCH",
+    url: `/api/memberships/${membership.id}`,
+    data: { channel }
+  })
+}
+
+export const deleteMembership = membershipId => {
+  return $.ajax({
+    method: "DELETE",
+    url: `/api/memberships/${membershipId}`
+  })
+}
